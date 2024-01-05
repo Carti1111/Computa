4cc5cb1277cdcbf047e4b113338487bcfb7d9367
LIMITBREAKER <143012069+Lunarix1111@users.noreply.github.com>
2024-01-05 21:13:50 +0000

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,7 @@
+HOSTNAME = "HOSTNAME" # ex: C:/Users/QwertyL0L/Downloads/example.py
+
+TOKEN = "YOUR BOT TOKEN" # replace with your totekn
+
+PREFIX = "c!" # replace with your prefix
+
+USER_ID = 898358037239201812 # replace with your user id

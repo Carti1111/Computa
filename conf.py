7a7175a1418dfcee097677055b98cd96e7bfa315
LIMITBREAKER <143012069+Lunarix1111@users.noreply.github.com>
2024-01-05 21:17:09 +0000

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,7 +1,7 @@
-HOSTNAME = "HOSTNAME" # ex: C:/Users/QwertyL0L/Downloads/example.py
+HOSTNAME = "HOSTNAME" # ex: C:/Users/sajun/Downloads/example.py
 
 TOKEN = "YOUR BOT TOKEN" # replace with your totekn
 
-PREFIX = "c!" # replace with your prefix
+PREFIX = "l!" # replace with your prefix
 
-USER_ID = 898358037239201812 # replace with your user id
+USER_ID = 1180468867739422742 # replace with your user id
